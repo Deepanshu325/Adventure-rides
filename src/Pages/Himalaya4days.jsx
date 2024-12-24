8fef1857212020502b2cc4a5d447920abd8341d0
Deepanshu325 <deepanshushrivastav999@gmail.com>
2024-12-24 15:49:07 +0530

    57th commit

--- src/Pages/Himalaya4days.jsx
+++ src/Pages/Himalaya4days.jsx
@@ -166,7 +166,7 @@ const Himalaya4days = () => {
             <div>
               <button
                 type="submit"
-                className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                className="w-[80%] bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
               >
                 {t("submit")}
               </button>
